358e0d5de52bd1fcb2cbbab77e77c019e24fc871
MohammadHH <mohammad19991998dtrb@gmail.com>
2021-01-11 06:10:17 +0200

    Updates

---
+++ precache-manifest.aa43114dfa44a2cbb10f73c0122deb95.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4828af2a8d712b90ae7272b7bf73102f",
+    "url": "/book-shelf/index.html"
+  },
+  {
+    "revision": "e58cad35b1f34436e682",
+    "url": "/book-shelf/static/css/2.c2ad9185.chunk.css"
+  },
+  {
+    "revision": "ef7478b49e02d7ccb340",
+    "url": "/book-shelf/static/css/main.0c3618a2.chunk.css"
+  },
+  {
+    "revision": "e58cad35b1f34436e682",
+    "url": "/book-shelf/static/js/2.3578e7f7.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/book-shelf/static/js/2.3578e7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7478b49e02d7ccb340",
+    "url": "/book-shelf/static/js/main.dc850f7e.chunk.js"
+  },
+  {
+    "revision": "7c58b79b0e8bcc834d7f",
+    "url": "/book-shelf/static/js/runtime-main.27057583.js"
+  },
+  {
+    "revision": "8563f3f676e11958bec1b483859d5575",
+    "url": "/book-shelf/static/media/Audio.8563f3f6.png"
+  },
+  {
+    "revision": "e8ccdfee0a3b84d13aa0b9e8605c38c8",
+    "url": "/book-shelf/static/media/BalancingAct.e8ccdfee.png"
+  },
+  {
+    "revision": "414ab8c1cc5c2b97c9a20eb593b74626",
+    "url": "/book-shelf/static/media/BookThief.414ab8c1.png"
+  },
+  {
+    "revision": "62f540cc2d53fea34721b4bf4fbbc3a7",
+    "url": "/book-shelf/static/media/Bootlegger.62f540cc.png"
+  },
+  {
+    "revision": "68b42d0fa9dbbc49d6440370dda5a47c",
+    "url": "/book-shelf/static/media/Business.68b42d0f.png"
+  },
+  {
+    "revision": "da286e4887263ba5cd9dbf73762c8f58",
+    "url": "/book-shelf/static/media/Children.da286e48.png"
+  },
+  {
+    "revision": "f2c4f8bfb50bd02cb81ddffe8e08fef4",
+    "url": "/book-shelf/static/media/Cooking.f2c4f8bf.png"
+  },
+  {
+    "revision": "dc1dac7278d7a12eb821c2a5aca23ee0",
+    "url": "/book-shelf/static/media/DecentHour.dc1dac72.png"
+  },
+  {
+    "revision": "a3696456894d44e27677d0e9bac65a4d",
+    "url": "/book-shelf/static/media/DirtyWood.a3696456.png"
+  },
+  {
+    "revision": "255496bf49d389ad927d420af49bf353",
+    "url": "/book-shelf/static/media/FairyThings.255496bf.png"
+  },
+  {
+    "revision": "dfa8c69dfce419263ae01181f67dfaba",
+    "url": "/book-shelf/static/media/Fever.dfa8c69d.png"
+  },
+  {
+    "revision": "705addc1d62494cb57c3ccee198f7fed",
+    "url": "/book-shelf/static/media/FeverTree.705addc1.png"
+  },
+  {
+    "revision": "a68455e95dbb0899c647a213f9616150",
+    "url": "/book-shelf/static/media/Fiction.a68455e9.png"
+  },
+  {
+    "revision": "eeb37bc777de2a602ef3ca6881aada7e",
+    "url": "/book-shelf/static/media/GreenWald.eeb37bc7.png"
+  },
+  {
+    "revision": "9587d29c925f94e2b84d9ed30fbfd432",
+    "url": "/book-shelf/static/media/InsideTheZoo.9587d29c.png"
+  },
+  {
+    "revision": "3bb8de432bbb2bad0a4b50ad136a21f5",
+    "url": "/book-shelf/static/media/JohnMarsden.3bb8de43.png"
+  },
+  {
+    "revision": "9e965de7db2523d66be64f58461a5b33",
+    "url": "/book-shelf/static/media/LifeStyle.9e965de7.png"
+  },
+  {
+    "revision": "87805d1fbeac21810d3b1b4143296047",
+    "url": "/book-shelf/static/media/Mystery.87805d1f.png"
+  },
+  {
+    "revision": "c51536c246a676428d132886452439ce",
+    "url": "/book-shelf/static/media/PhonicsGames.c51536c2.png"
+  },
+  {
+    "revision": "6ab9f90d8605dad9ca26fd4432a3b95e",
+    "url": "/book-shelf/static/media/Romance.6ab9f90d.png"
+  },
+  {
+    "revision": "5bf86d20ba6db3b721f042d532dd6ca3",
+    "url": "/book-shelf/static/media/RosieProject.5bf86d20.png"
+  },
+  {
+    "revision": "41b03eeef8fa6755d28ff2f86e17913b",
+    "url": "/book-shelf/static/media/TheBook.41b03eee.png"
+  },
+  {
+    "revision": "535d65a7e23ec727f82d1f4427fb812b",
+    "url": "/book-shelf/static/media/TheBootlegger.535d65a7.png"
+  },
+  {
+    "revision": "37ea6c8089c59b91118c1755a59d6827",
+    "url": "/book-shelf/static/media/Unicorn.37ea6c80.png"
+  },
+  {
+    "revision": "8894d5a666a1d6578594cc587a0d7ba9",
+    "url": "/book-shelf/static/media/WilliamShakespeare.8894d5a6.png"
+  },
+  {
+    "revision": "64274aa4af1b01894cbd1e3b5836d04d",
+    "url": "/book-shelf/static/media/WishMeDead.64274aa4.png"
+  },
+  {
+    "revision": "f1c402dfcb379bd05dd35f321ae2fd69",
+    "url": "/book-shelf/static/media/carousel-background.f1c402df.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/book-shelf/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/book-shelf/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/book-shelf/static/media/revicons.2feb69cc.eot"
+  }
+]);
