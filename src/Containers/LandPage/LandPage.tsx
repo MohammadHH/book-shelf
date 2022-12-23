0ccd437546b74280ff9933b5139721170643ea80
MohammadHH <mohammad19991998dtrb@gmail.com>
2022-12-23 23:21:30 +0200

    feat(LandPage): upgrade landing page container

---
+++ src/Containers/LandPage/LandPage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { Categories } from "../../layouts/Categories/Categories";
+import { BooksCarousel } from "../../layouts/CarouselList/BooksCarousel";
+import { CategoryBooks, fetchBooksByCategories } from "../../services/booksAPI";
+import { Category } from "../../services/categoriesAPI";
+
+interface LandPageProps {
+  categories: Category[];
+}
+
+export function LandPage({ categories }: LandPageProps) {
+  const [booksList, setBooksList] = useState<CategoryBooks[]>([]);
+  useEffect(() => {
+    fetchBooksByCategories().then((books) => setBooksList(books || []));
+  }, []);
+
+  return (
+    <>
+      <Categories categories={categories} />
+      {booksList.map((bookList, index) => (
+        <BooksCarousel
+          {...bookList}
+          even={index % 2 === 0}
+          key={bookList.category}
+        />
+      ))}
+    </>
+  );
+}
+
+export default LandPage;
