41d3fa8782d140c6694c897a227e384fb61d9e26
MohammadHH <mohammad19991998dtrb@gmail.com>
2022-12-24 04:25:37 +0200

    fix(Brand): fix brand styling

--- src/components/Brand/Brand.tsx
+++ src/components/Brand/Brand.tsx
@@ -3,9 +3,9 @@ import styles from "./Brand.module.scss";
 
 export function Brand() {
   return (
-    <Link className={styles["navbar-brand"]} to="/" id="logo">
+    <Link className={styles["navbar-brand"]} to="/">
       <h1>
-        Books<span className={styles["brand-reviews"]}>Reviews</span>
+        Books<span className={styles["brand--thin"]}>Reviews</span>
       </h1>
     </Link>
   );
